8c2f0c9238cf9b5740dca5eda53741105dee57a6
SilviM77 <81195514+SilviM77@users.noreply.github.com>
2021-12-22 17:17:06 -0500

    Added css file

---
+++ src/script.js
@@ -0,0 +1,91 @@
+function formatDate(date) {
+    let currentDate = date.getDate();
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  
+    let monthIndex = date.getMonth();
+    let months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec"
+    ];
+    let month = months[monthIndex];
+  
+    let dayIndex = date.getDay();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+    let day = days[dayIndex];
+  
+    return `Today is ${day} ${currentDate}, ${month}, ${hours}:${minutes}`;
+  }
+  let dateElement = document.querySelector("#date");
+  let now = new Date();
+  dateElement.innerHTML = formatDate(now);
+  
+  function showWeather(response) {
+    document.querySelector("#city").innerHTML = response.data.name;
+    document.querySelector("#temperature").innerHTML = Math.round(
+      response.data.main.temp
+    );
+    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+    document.querySelector("#wind").innerHTML = Math.round(
+      response.data.wind.speed
+    );
+    document.querySelector("#description").innerHTML =
+      response.data.weather[0].main;
+  }
+  
+  function searchCity(city) {
+    let apiKey = "bd8267e0e5535d9938ff03155812f193";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(showWeather);
+  }
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    let city = document.querySelector("#city-input").value;
+    searchCity(city);
+  }
+  
+  function searchLocation(position) {
+    let apiKey = "bd8267e0e5535d9938ff03155812f193";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(showWeather);
+  }
+  
+  function getCurrentLocation(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(searchLocation);
+  }
+  
+  let searchForm = document.querySelector("#search-form");
+  searchForm.addEventListener("submit", handleSubmit);
+  
+  let currentLocationButton = document.querySelector("#current-location-button");
+  currentLocationButton.addEventListener("click", getCurrentLocation);
+  
+  searchCity("New York");
+  
